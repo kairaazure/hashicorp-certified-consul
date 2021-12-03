06019edcba5be00f8db53c5418f3b04b05266cd4
kairaazure <kairaazbansal@gmail.com>
2021-12-03 13:24:25 +0530

    added commands

---
+++ commands.sh
@@ -0,0 +1,19 @@
+macOS Installation 
+   brew tap hashicorp/tap
+   brew install hashicorp/tap/consul
+   brew upgrade hashicorp/tap/consul
+   consul agent -dev #to start the dev agent
+   http://localhost:8500/ui  #consul ui
+   stop the consul dev agent to move back to the original consul # it removes all the previous data
+
+#Others Commands
+
+ 1004  netstat | grep consul
+ 1005  netstat | grep consul
+ 1006  netstat
+ 1007  clear
+ 1008  ls 
+ 1009  consul members
+ 1010  consul members
+ 1011  consul members -detailed
+ 1012  curl localhost:8500/v1/catalog/nodes
